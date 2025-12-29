63f622ca460c7331d9c3ab5e777a173271aa36ca
mastersheep <vinhluucongk17@gmail.com>
2025-12-29 14:57:14 +0700

    add train module

---
+++ train_new.py
@@ -0,0 +1,106 @@
+from temporalgan.gen_s2_lc_v1_0 import Generator
+from temporalgan.disc_s2_lc_v1_0 import Discriminator
+from dataset.o2s_dataset import O2SDataset
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose, RandomHorizontalFlip, ToTensor, Normalize
+import sys
+from pathlib import Path
+import yaml
+from argparse import ArgumentParser
+import torch
+from torch.optim import Adam
+from datetime import datetime
+import os
+from torch.utils.tensorboard import SummaryWriter
+from trainer import Trainer
+
+def load_config(config_path: str):
+    try:
+        with open(Path(config_path), 'r') as file:
+            config = yaml.safe_load(file)
+            return config
+    except FileNotFoundError as e:
+        print(f'{e}: Your config path is not valid.')
+        sys.exit(1)
+
+if __name__ == '__main__':
+    parser = ArgumentParser(prog="Model Training")
+    parser.add_argument('--config_path', type=str, required=True)
+    
+    args = parser.parse_args()
+    #---------Load yaml file---------------
+
+    config_dict = load_config(args.config_path)
+    cfg_data = config_dict['data']
+    cfg_train = config_dict['train']
+
+    #---------Load checkpoint---------------
+    ckp_path = cfg_train['resume_path']
+    if ckp_path:
+        ckp_path = Path(ckp_path)
+        if not os.path.exists(ckp_path):
+            raise FileNotFoundError(f'Resume_path: {ckp_path} is not valid!')
+        try: 
+            run_name = ckp_path.parent.name
+        except Exception as e:
+            print(f'Cannot extract run_name from {ckp_path}')
+            sys.exit(1)
+    else:
+        run_name = f'exp_{datetime.now().strftime("%Y%m%d-%H%M%S")}'
+    
+    #-----------Init Summary Writer to log-------------
+    logdir = os.path.join('runs', run_name)
+    os.makedirs(logdir, exist_ok=True)
+    writer = SummaryWriter(log_dir = logdir)
+    print(f'TensorBoard logs will be saved in {logdir}')
+
+
+    #-------------Dataset, Dataloader-----------
+
+    transform_RGB = Compose(transforms=[
+        ToTensor(),
+        Normalize(mean=[0.5, 0.5, 0.5], std = [0.5, 0.5, 0.5])
+    ])
+
+    transform_gray = Compose(transforms=[
+        ToTensor(),
+        Normalize(mean=[0.5], std=[0.5])
+    ])
+    
+        #------Dataset--------------
+    train_set = O2SDataset(train=True, transform_rgb=transform_RGB, transform_gray= transform_gray, cfg_data=cfg_data)
+    valid_set = O2SDataset(valid=True, transform_rgb=transform_RGB, transform_gray=transform_gray, cfg_data=cfg_data)
+    test_set = O2SDataset(test=True, transform_rgb=transform_RGB, transform_gray=transform_gray, cfg_data=cfg_data)
+
+
+        #--------Dataloader----------
+    train_loader = DataLoader(
+        dataset=train_set,
+        batch_size=cfg_data['train_batch_size'],
+        shuffle=True,
+        num_workers=cfg_data['num_workers'],
+        drop_last=True
+    )
+
+    valid_loader = DataLoader(
+        dataset=valid_set,
+        batch_size=cfg_data['valid_batch_size'],
+        shuffle=False,
+        num_workers=cfg_data['num_workers'],
+        drop_last=True
+    )
+
+
+    #---------Configure model------------
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    netG = Generator(s2_in_channels=3, lc_in_channels=3, out_channels=1, features=64)
+    netD = Discriminator(s2_in_channels=3, lc_in_channels=3, s1_out_channels=1)
+    optG = Adam(netG.parameters(), lr = cfg_train['lr'], betas=tuple(cfg_train['betas']))
+    optD = Adam(netD.parameters(), lr = cfg_train['lr'], betas=tuple(cfg_train['betas']))
+
+
+    #----------Train-----------
+    train = Trainer(netG, netD, optG, optD, train_loader, valid_loader, device, config_dict, writer, run_name, ckp_path)
+    train.run()
+
+
--- trainer.py
+++ trainer.py
@@ -1,27 +1,35 @@
 import os
 import torch
-from datetime import datetime
+import sys
+# from datetime import datetime
 from tqdm import tqdm
 # from src import l1_loss, lratio_loss, psnr_torch, ssim_torch, gradient_loss
 from torch.nn import BCEWithLogitsLoss
 from torch.nn import L1Loss
+from ignite.metrics import PSNR, SSIM
+from ignite.engine import Engine
+from torchvision.utils import make_grid
+
+def denorm(x):
+    return (x * 0.5 + 0.5).clamp(0, 1)
 
 class Trainer():
     def __init__(self, netG, netD, optG, optD, train_loader, valid_loader, device, config, writer, run_name, resume_path = None, kaggle = None):
-        # config
+        # Config
         self.config = config
         self.train_cfg = self.config['train']
-        self.model_cfg = self.config['model']
+        # self.model_cfg = self.config['model']
         self.val_step = self.train_cfg['val_step']
-        
+        self.total_epochs = self.train_cfg['total_epochs']
+
         # Model
-        self.netG = netG
-        self.netD = netD
+        self.device = device
+        self.netG = netG.to(self.device)
+        self.netD = netD.to(self.device)
         self.optG = optG
         self.optD = optD
-        self.device = device
-
-        # coefficent loss
+        
+        # l1's coefficent 
         self.lambda_l1 = self.train_cfg['lambda_l1']
 
         # Loader
@@ -30,6 +38,7 @@ class Trainer():
         self.num_iter_train = len(self.train_loader)
         self.num_iter_valid = len(self.valid_loader)
 
+        self.total_steps = self.total_epochs * self.num_iter_train
         
         # log, checkpoint
         self.writer = writer
@@ -40,11 +49,16 @@ class Trainer():
             self.checkpoint_dir = '/kaggle/working/' + self.checkpoint_dir
 
         os.makedirs(self.checkpoint_dir, exist_ok=True)
-        self.start_epoch = 1
-        self.best_psnr = 0.0
-        self.loss_show = 0.0
-        self.total_epochs = self.train_cfg['epochs']
-        self.end_epoch = self.start_epoch + self.total_epochs
+        
+        self.current_step = 0
+        self.best_ssim = 0.0
+
+        # losses
+        self.criterion_GAN = BCEWithLogitsLoss()
+        self.criterion_L1 = L1Loss()
+        self.loss_G = 0.0
+        self.loss_l1 = 0.0
+        self.loss_D = 0.0
 
         if self.resume_path:
             self._load_checkpoint(resume_path)
@@ -54,23 +68,29 @@ class Trainer():
         try:
             # Load checkpoint
             checkpoint = torch.load(resume_path)
-            self.model.load_state_dict(checkpoint['model_state_dict'])
-            self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-            self.start_epoch = checkpoint['epoch'] + 1
-            self.end_epoch = self.start_epoch + self.total_epochs
-            self.best_psnr = checkpoint['best_psnr']
-        
+            self.netG.load_state_dict(checkpoint['netG_state_dict'])
+            self.netD.load_state_dict(checkpoint['netD_state_dict'])
+            self.optG.load_state_dict(checkpoint['optG_state_dict'])
+            self.optD.load_state_dict(checkpoint['optD_state_dict'])
+
+            self.current_step = checkpoint['step']
+            self.best_ssim = checkpoint.get('best_ssim', 0.0)
+
+            print(f"Resumed from step {self.current_step}. Best SSIM so far: {self.best_ssim}")
+
         except Exception as e:
             print(f'Error loading checkpoint {e}. Double check resume path')
-            exit()
+            sys.exit(1)
     
     
-    def _save_checkpoint(self, epoch:int, is_best: bool):
+    def _save_checkpoint(self, step:int, is_best: bool):
         checkpoint_data = {
-            'epoch':epoch,
-            'model_state_dict': self.model.state_dict(),
-            'optimizer_state_dict': self.optimizer.state_dict(),
-            'best_psnr': self.best_psnr,
+            'step': step,
+            'netG_state_dict': self.netG.state_dict(),
+            'netD_state_dict': self.netD.state_dict(),
+            'optG_state_dict': self.optG.state_dict(),
+            'optD_state_dict': self.optD.state_dict(),
+            'best_ssim': self.best_ssim,
             'config': self.config
         }
         # Save last checkpoint
@@ -81,137 +101,79 @@ class Trainer():
         if is_best:
             best_save_path = os.path.join(self.checkpoint_dir, 'best.pth')
             torch.save(checkpoint_data, best_save_path)
-            print(f"Epoch {epoch}: New best model saved to {best_save_path}")
+            print(f"Step {step}: New best model saved to {best_save_path}")
     
 
-    def _train_epoch(self, epoch: int):
+
+    def _validate_step(self, current_step):
         """
-        Process an training epoch
+        Process an validation by val_step
         """
-        self.model.train()
-        
-        tqdm_train_loader = tqdm(self.train_loader, desc=f'Epoch [{epoch}/{self.end_epoch-1}] Train')
-
-
-        def denorm(x):
-            return (x * 0.5 + 0.5).clamp(0, 1)
-        
-        for iter, ((s2, lc), s1) in enumerate(tqdm_train_loader):
-            current_global_step = (epoch - 1) * self.num_iter_train + iter
-            s2, lc = s2.to(self.device), lc.to(self.device)
-            s1 = s1.to(self.device)
-            # hr= denorm(hr)
-            
-            # forward pass
-            s1_fake = self.model(s2, lc)
-            
-            # train discriminator
-            for param in self.netD.parameters():
-                param.requires_grad = True
-            # compute loss of D
-            # real
-            D_real_output = self.netD(s2, lc, s1)
-            D_real_loss = BCEWithLogitsLoss(D_real_output, torch.ones_like(D_real_output))
-            
-            # fake
-            D_fake_output = self.netD(s2, lc, s1_fake.detach())
-            D_fake_loss = BCEWithLogitsLoss(D_fake_output, torch.zeros_like(D_fake_output))
-            
-            D_loss = (D_fake_loss + D_real_loss) / 2
-            # compute gradients
-            self.optD.zero_grad()
-            D_loss.backward()
-            # update weigts
-            self.optD.step()
-            
-            # Train Generator
-            for param in self.netD.parameters():
-                param.requires_grad = False
-            D_fake_output = self.netD(s2, lc, s1_fake)
-            G_fake_loss = BCEWithLogitsLoss(D_fake_output, torch.ones_like(D_fake_output))
-            G_l1_loss = L1Loss(s1_fake, s1)
-            G_loss = self.lambda_l1 * G_l1_loss + G_fake_loss
-
-            # compute gradients
-            self.optG.zero_grad()
-            G_loss.backward()
-            self.optG.step()
-
-
-            total_ratio_loss = 0.0
-            total_l1_loss = 0.0
-            total_grad_loss = 0.0
-            
-
-           
-            
-            
-            loss = (self.theta_1 * total_ratio_loss) + (self.theta_2 * total_l1_loss) + (self.theta_3 * total_grad_loss)
-
-            self.loss_show += loss.item()
-       
-            if iter % 100 == 0:
-                print(f"\nRatio Loss: {self.theta_1 * total_ratio_loss.item():.4f}, L1 Loss: {self.theta_2 * total_l1_loss.item():.4f}, Gradient Loss: {self.theta_3 * total_grad_loss.item():.4f}")
-            # backward pass
-            # Update tqdm
-            tqdm_train_loader.set_postfix(Loss=f'{loss.item():.5f}')
-            
-
-            # Validation by step
-            if (current_global_step + 1) % self.val_step == 0:
-                # Log loss each step
-                avg_loss = self.loss_show/self.val_step
-                print(f"Step [{(current_global_step+1)}], Average Train Loss: {avg_loss:.5f}")
-                self.loss_show = 0.0
-                self.writer.add_scalar(tag='Loss/Train_Step', scalar_value=avg_loss, global_step=current_global_step)
-
-                print(f"\n[Step {(current_global_step + 1)}] Start Validating...")
-                # Validate
-                avg_psnr, avg_ssim = self._validate_epoch(epoch)
-                # Log Metrics by Step
-                self.writer.add_scalar(tag='Metrics/PSNR', scalar_value=avg_psnr, global_step=current_global_step)
-                self.writer.add_scalar(tag='Metrics/SSIM', scalar_value=avg_ssim, global_step=current_global_step)
-
-                # Check best & Save Checkpoint
-                is_best = avg_psnr > self.best_psnr
-                if is_best:
-                    self.best_psnr = avg_psnr
-                    print(f"New Best PSNR: {self.best_psnr:.3f}")
-                
-                self._save_checkpoint(epoch, is_best)
-                self.model.train()
-
-       
-
-
-    def _validate_epoch(self, epoch: int):
-        """
-        Process an validation epoch
-        """
-        self.netG.eval()
-        psnr_sum = 0
-        ssim_sum = 0
-        tqdm_valid_loader = tqdm(self.valid_loader, desc=f'Epoch [{epoch}/{self.end_epoch-1}] Valid')
-        with torch.no_grad():
-            for iter, ((s2, lc), s1) in enumerate(tqdm_valid_loader):
-                s2, lc = s2.to(self.device), lc.to(self.device)
-                s1= s1.to(self.device)
-                # forward pass
+        def eval_step(engine, batch):
+            self.netG.eval()
+            with torch.no_grad():
+                (s2, lc), s1 = batch
+                s2, lc, s1 = s2.to(self.device), lc.to(self.device), s1.to(self.device)
                 s1_fake = self.netG(s2, lc)
 
-                # calculate metrics
-                psnr_batch = psnr_torch(sr_fusion, hr).item()
-                ssim_batch = ssim_torch(sr_fusion, hr).item()
-                psnr_sum += psnr_batch
-                ssim_sum += ssim_batch
+            return denorm(s1_fake), denorm(s1), denorm(s2)
+        
+        # compute loss of train set
+        lossG_avg = self.loss_G / self.val_step
+        lossL1_avg = self.loss_l1 / self.val_step
+        lossD_avg = self.loss_D / self.val_step
 
-            avg_psnr = psnr_sum/self.num_iter_valid
-            avg_ssim = ssim_sum/self.num_iter_valid
-            print(f'Epoch [{epoch}/{self.end_epoch-1}] Valid\nPSNR: {avg_psnr:.3f} dB\nSSIM: {avg_ssim:.3f}')
+        print(f"""Step [{current_step}/{self.total_steps}]
+            \nAverage Train L1 Loss: {lossL1_avg:.3f}
+            \nAverage Train G Loss: {lossG_avg:.3f}
+            \nAverage Train D Loss: {lossD_avg:.3f}
+                """)
+        print(f'Start Validating...')
         
+        # compute metrics of valid set
+        evaluator = Engine(eval_step)
+        PSNR(data_range=1.0, output_transform=lambda x: (x[0], x[1])).attach(evaluator, 'psnr')
+        SSIM(data_range=1.0, output_transform=lambda x: (x[0], x[1])).attach(evaluator, 'ssim')
+        evaluator.run(tqdm(self.valid_loader, desc="Validating", leave=False))
+        psnr_avg = evaluator.state.metrics['psnr']
+        ssim_avg = evaluator.state.metrics['ssim']
+        print(f'PSNR: {psnr_avg:.3f}db\nSSIM: {ssim_avg:.3f}')
+
+        # log
+        self.writer.add_scalar(tag = 'L1 Loss/Train_Step', scalar_value = lossL1_avg, global_step = current_step)
+        self.writer.add_scalar(tag = 'G Loss/Train_Step', scalar_value = lossG_avg, global_step = current_step)
+        self.writer.add_scalar(tag = 'D Loss/Train_Step', scalar_value = lossD_avg, global_step = current_step)
+        self.writer.add_scalar(tag='Metrics/PSNR', scalar_value = psnr_avg, global_step = current_step)
+        self.writer.add_scalar(tag='Metrics/SSIM', scalar_value = ssim_avg, global_step = current_step)
+
+        # Log images
+        fake, real, inp = evaluator.state.output
+        n_imgs = min(8, fake.size(0))
         
-        return avg_psnr, avg_ssim
-    
+        # def to_vis(x):
+        #     if x.shape[1] == 2: return x[:, :1] # Take first channel if 2 channels (e.g. VV)
+        #     if x.shape[1] > 3: return x[:, :3]  # Take first 3 channels if > 3 (e.g. RGB bands)
+        #     return x
+
+        self.writer.add_image('Images/Fake', make_grid((fake)[:n_imgs], nrow=4), current_step)
+        self.writer.add_image('Images/Real', make_grid((real)[:n_imgs], nrow=4), current_step)
+        self.writer.add_image('Images/Input', make_grid((inp)[:n_imgs], nrow=4), current_step)
+
+        # Check best ssim and save checkpoint
+        is_best_ssim = ssim_avg > self.best_ssim
+        if is_best_ssim:
+            self.best_ssim = ssim_avg
+            print(f'New best SSIM: {self.best_ssim:.3f}')
+        else:
+            print(f'Latest checkpoint saved!')
+        
+        self._save_checkpoint(current_step, is_best_ssim)
+
+        # Reset loss
+        self.loss_D = 0.0
+        self.loss_G = 0.0
+        self.loss_l1 = 0.0
+
 
     def run(self):
         if not self.resume_path:
@@ -220,15 +182,93 @@ class Trainer():
                 """)
         else:
             print(f"""------------------
-                  \nResuming run '{self.run_name}' from epoch {self.start_epoch}. Best PSNR: {self.best_psnr:.3f}
+                  \nResuming run '{self.run_name}' from step {self.current_step}.
                 """)
         
-        for epoch in range(self.start_epoch, self.end_epoch):
-            torch.cuda.empty_cache()
+
+        train_iter = iter(self.train_loader)
+
+        pbar = tqdm(total=self.total_steps, initial=self.current_step, desc='Training')
+
+        #-----------------MAIN--------------------------
+        while self.current_step < self.total_steps:
             
-            # training
-            self._train_epoch(epoch)
+            try:
+                (s2, lc), s1= next(train_iter)
+
+            except StopIteration:
+                train_iter = iter(self.train_loader)
+                (s2, lc), s1 = next(train_iter)
+
+            s2, lc, s1 = s2.to(self.device), lc.to(self.device), s1.to(self.device)
             
+            # generate S1 fake
+            s1_fake = self.netG(s2, lc)
+
+
+            #---------Start Training---------------
+            self.netG.train()
+            self.netD.train()
+
+
+            #------------ Train Discriminator--------------
+            for param in self.netD.parameters():
+                param.requires_grad = True
+            
+            self.optD.zero_grad()
+            # Loss of fake
+            D_fake_output = self.netD(s2, lc, s1_fake.detach())
+            D_fake_loss = self.criterion_GAN(D_fake_output, torch.zeros_like(D_fake_output))
+            
+            # Loss of real
+            D_real_output = self.netD(s2, lc, s1)
+            D_real_loss = self.criterion_GAN(D_real_output, torch.ones_like(D_real_output))
+
+            D_losses = (D_fake_loss + D_real_loss) / 2 # Losses average
+            D_losses.backward() # Compute gradients
+            self.optD.step() # Update weights
+
+
+            #--------------Train Generator-----------------
+            for param in self.netD.parameters():
+                param.requires_grad = False
+            
+            self.optG.zero_grad()
+            # Loss by D
+            D_fake_output = self.netD(s2, lc, s1_fake)
+            G_gan_loss = self.criterion_GAN(D_fake_output, torch.ones_like(D_fake_output))
+
+            # Loss by L1
+            G_l1_loss = self.criterion_L1(s1_fake, s1)
+
+            # Losses sum
+            G_total_loss = self.lambda_l1 * G_l1_loss + G_gan_loss
+            
+            # Update
+            G_total_loss.backward()
+            self.optG.step()
+            #----End 1 iter/step train
+
+
+            #---------------Logging and Update-----------------------
+            self.loss_G += G_gan_loss.item()
+            self.loss_l1 += G_l1_loss.item()
+            self.loss_D += D_losses.item()
+
+            pbar.set_postfix({
+                'D_GAN': f'{D_losses.item():.3f}',
+                'G_GAN': f'{G_gan_loss.item():.3f}',
+                'L1': f'{G_l1_loss.item():.3f}'
+            })
+
+            self.current_step += 1
+            pbar.update(1)
+
+
+            #---------------Validate----------------------
+            if self.current_step % self.val_step == 0:
+                self._validate_step(self.current_step)
+
         self.writer.close()
         print("Training finished.")
             
@@ -238,4 +278,3 @@ class Trainer():
 
 
         
-
